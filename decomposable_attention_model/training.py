7d3f08797bcaad5688cea5fdf7ac5d655a7e109a
Yuta Koreeda <koreyou@mac.com>
2016-12-31 16:20:14 +0900

    Added short reporting to training. Also, added learning rate decay.

--- decomposable_attention_model/training.py
+++ decomposable_attention_model/training.py
@@ -48,11 +48,16 @@ def forward_pred(model, dataset, device=None):
 
 
 def train(model, optimizer, train_itr, n_epoch, dev=None, device=None,
-          tmp_dir='tmp.model'):
+          tmp_dir='tmp.model', short_report=None, lr_decay=0.9):
     loss = 0.
     acc = 0.
+    l = None
+    a = None
     min_loss = float('inf')
     min_epoch = 0
+    num_steps = len(train_itr.dataset) / float(train_itr.batch_size)
+    lr_decay = lr_decay ** (1. / num_steps)
+    report_short_tmpl = "[   ] T/loss={:0.4f} T/acc={:0.2f}"
     report_tmpl = "[{:>3d}] T/loss={:0.4f} T/acc={:0.2f} D/loss={:0.4f} D/acc={:0.2f}"
     for batch in train_itr:
         if train_itr.is_new_epoch:
@@ -69,10 +74,15 @@ def train(model, optimizer, train_itr, n_epoch, dev=None, device=None,
 
             loss = 0.
             acc = 0.
+        elif (short_report is not None and
+              train_itr.current_position % short_report < train_itr.batch_size
+              and l is not None):
+            logging.info(report_short_tmpl.format(l, a))
         if train_itr.epoch == n_epoch:
             break
         l, a, _ = _run_batch(model, optimizer, batch, device, True)
         loss += l * len(batch)
         acc += a * len(batch)
+        optimizer.alpha *= lr_decay
     logging.info('loading early stopped-model at epoch {}'.format(min_epoch))
     chainer.serializers.load_npz(tmp_dir, model)
